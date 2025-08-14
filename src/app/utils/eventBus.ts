7929e72f4f517dbd735ab85f6be73cc7a6617724
shijie <dan.cancer@gmail.com>
2025-08-14 18:14:20 +0800

    refactor(utils): 移除eventBus中未使用的类型定义
    清理未使用的componentSelected和addComponentType类型，简化事件类型定义

--- src/app/utils/eventBus.ts
+++ src/app/utils/eventBus.ts
@@ -5,13 +5,11 @@ import { ComponentData } from '../common/types';
 type EventBusEvents = {
   updateComponentStyle: { componentId: number; styleUpdates: Record<string, unknown> };
   updateComponentProps: { componentId: number; propsUpdates: Record<string, number | number[] | string | string[] | undefined> };
-  componentSelected: any;
   addComponent: { componentType: string; position: { x: number; y: number }; parentComponentId?: number };
   addChildComponent: AddChildComponentData;
   zoomCanvas: string;
   updateComponentTree: { componentTree: ComponentData[]; root: ComponentData };
   selectComponent: number;
-  addComponentType: any;
 };
 
 export const eventBus = mitt<EventBusEvents>();
